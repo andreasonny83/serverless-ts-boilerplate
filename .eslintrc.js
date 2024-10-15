095df83ddce085174b8aa4fc054f84232c119aca
andreasonny83 <andreasonny83@gmail.com>
2024-10-15 18:21:41 +0100

    refactor

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
-  plugins: ['@typescript-eslint'],
-};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ['src/*.ts'] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
